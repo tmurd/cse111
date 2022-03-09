b5da96812ecd84639b0ef5e60871a8b77e114986
Trenton Murdock <y4trenton@gmail.com>
2022-03-09 10:57:17 -0700

    prove assignment

--- receipt.py
+++ receipt.py
@@ -1,6 +1,15 @@
 # Import CSV Modual
 import csv
 
+# Import the datetime class from the datetime
+# module so that it can be used in this program.
+from datetime import datetime
+
+# Call the now() method to get the current
+# date and time as a datetime object from
+# the computer's operating system.
+current_date_and_time = datetime.now()
+
 # Define product indexes
 PRODUCT_NUMBER_INDEX = 0
 PRODUCT_NAME_INDEX = 1
@@ -10,45 +19,75 @@ RETAIL_PRICE_INDEX = 2
 QUANTITY_INDEX = 1
 
 def main():
+    try:
+        # Read and store file
+        # Use to insert into read_dict function
+        product_dict = read_dict("products.csv", PRODUCT_NUMBER_INDEX)
 
-    # Read and store file
-    # Use to insert into read_dict function
-    product_dict = read_dict("products.csv", PRODUCT_NUMBER_INDEX)
+        #print_product_dict_neat(product_dict)
 
-    print_product_dict_neat(product_dict)
+        print()
+        print("Inkom Emporium")
 
-    print()
-    print("Requested Items")
-    with open("request.csv", "rt") as csv_file:
-        
-        reader = csv.reader(csv_file)
+        print()
+        #print("Requested Items")
 
-        next(reader)
+        with open("request.csv", "rt") as csv_file:
 
-        for row_list in reader:
-        # Use the requested product number to find the corresponding item in the products_dict.
+            reader = csv.reader(csv_file)
 
-            # Establish product_number_index as key to use in product dictionary
-            key = row_list[PRODUCT_NUMBER_INDEX]
+            next(reader)
 
-            # Identify quantity_index as product quantity requested
-            quantity = row_list[QUANTITY_INDEX]
+            num_items = 0
+            subtotal = 0
 
-            # Match key to value in product_dict
-            value = product_dict[key]
+            for row_list in reader:
+            # Use the requested product number to find the corresponding item in the products_dict.
 
-            # Use value to find product name in product_dict
-            product_name = value[PRODUCT_NAME_INDEX]
+                # Establish product_number_index as key to use in product dictionary
+                key = row_list[PRODUCT_NUMBER_INDEX]
 
-            # Use value to find retail price in product_dict
-            price = value[RETAIL_PRICE_INDEX]
+                # Identify quantity_index as product quantity requested
+                quantity = int(row_list[QUANTITY_INDEX])
 
-        
-            # Print the product name, requested quantity, and product price.
-            print(f"{product_name}: {quantity} @ {price}")
-    
-    print()
+                # Match key to value in product_dict
+                value = product_dict[key]
 
+                # Use value to find product name in product_dict
+                product_name = value[PRODUCT_NAME_INDEX]
+
+                # Use value to find retail price in product_dict
+                price = float(value[RETAIL_PRICE_INDEX])
+
+                # Print the product name, requested quantity, and product price.
+                print(f"{product_name}: {quantity} @ {price}")
+
+                num_items += quantity
+                subtotal += price * quantity
+
+        sales_tax = round(subtotal * 0.06,2)
+        total = sales_tax + subtotal
+
+        print()
+        print(f"Number of Items: {num_items}")
+        print(f"Subtotal: {subtotal:.2f}")
+        print(f"Sales Tax: {sales_tax}")
+        print(f"Total: {total}")
+
+        print()
+        print("Thank you for shopping at Inkom Emporium")
+        # Print the current day of the week and the current time.
+        print(f"{current_date_and_time:%a %b  %w %X %Y}")
+        print()
+
+    except (FileNotFoundError, PermissionError) as error:
+        print("Error: missing file")
+        print(error)
+
+    except (KeyError) as error:
+        print()
+        print(f"Error: unknown product ID in the request.csv file '{key}'")
+        print()
 
 def read_dict(filename, key_column_index):
     """Read the contents of a CSV file into a compound
