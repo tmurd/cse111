d85af62e18c3ec6427bac03d850ad535da871f02
Trenton Murdock <y4trenton@gmail.com>
2022-04-02 19:27:18 -0600

    organized

--- ex.py
+++
@@ -1,9 +0,0 @@
-students = {
-    "10-450-1203": "Jorge Soares",
-    "75-421-2310": "Abdul Ali",
-    "07-103-5621": "Michelle Davis"
-}
-name = students["75-421-2310"]
-
-print(students)
-print(name)
